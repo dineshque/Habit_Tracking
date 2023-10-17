d8a063bb108794f9a41ff5aa49c482a85c765c58
Dinesh Kumar <79956122+dineshque@users.noreply.github.com>
2023-10-17 14:39:29 +0530

    Update main.py

--- Habit_Tracking/main.py
+++ Habit_Tracking/main.py
@@ -267,4 +267,5 @@ def delete_log(log_id):
 #====================================================================================
 
 if __name__=='__main__':
-    app.run(debug = True)
+    app.run(debug=True, port=5001)
+
